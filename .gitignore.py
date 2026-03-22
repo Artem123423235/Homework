dc177c5c37715dad4459d0114a3ac0ae21a98590
Артем <akarandaiev@bk.ru>
2026-03-22 19:44:06 +0300

    добавлен н6овый файл

---
+++ .gitignore.py
@@ -0,0 +1,22 @@
+# Byte-compiled / caches
+__pycache__/
+*.py[cod]
+*$py.class
+
+# Environments
+env/
+venv/
+.venv/
+
+# IDE
+.vscode/
+.idea/
+
+# OS
+.DS_Store
+Thumbs.db
+
+# Python packaging
+dist/
+build/
+*.egg-info/
